dbf46609b4094d52dd0579ae41315c3bb9e9a021
hadis_rezaeemanesh <h.rezee1991@gmail.com>
2022-01-03 22:18:18 +0330

    show related products in ProductPageFragment

--- app/src/main/java/com/example/onlineshop/view/fragment/ProductPageFragment.java
+++ app/src/main/java/com/example/onlineshop/view/fragment/ProductPageFragment.java
@@ -8,6 +8,8 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.onlineshop.R;
+import com.example.onlineshop.adapter.ProductAdapter;
 import com.example.onlineshop.adapter.SliderViewPagerAdapter;
 import com.example.onlineshop.databinding.FragmentProductDetailBinding;
 import com.example.onlineshop.model.Product;
@@ -24,6 +27,7 @@ import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnima
 import com.smarteist.autoimageslider.SliderAnimations;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProductPageFragment extends Fragment {
 
@@ -65,6 +69,13 @@ public class ProductPageFragment extends Fragment {
             }
         });
 
+        mPageViewModel.getRelatedProducts().observe(this, new Observer<List<Product>>() {
+            @Override
+            public void onChanged(List<Product> products) {
+                setAdapter(products);
+            }
+        });
+
     }
 
     @Override
@@ -74,7 +85,8 @@ public class ProductPageFragment extends Fragment {
         mBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_product_detail, container, false);
 
-
+        initViews();
+        navListeners();
 
         return mBinding.getRoot();
     }
@@ -85,11 +97,36 @@ public class ProductPageFragment extends Fragment {
 
     }
 
+    private void navListeners(){
+        mProductViewModel.getClickedItem().observe(getViewLifecycleOwner(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("productId", integer);
+                Navigation.findNavController(mBinding.getRoot()).navigate(R.id.productPageFragment, bundle);
+            }
+        });
+    }
+
     private void setupSliderAdapter(ArrayList<String> images) {
         mBinding.productDetailSliderView.setSliderAdapter(new SliderViewPagerAdapter(getContext(), images));
         mBinding.productDetailSliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
         mBinding.productDetailSliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
         mBinding.productDetailSliderView.startAutoCycle();
+    }
+
+    private void setAdapter(List<Product> products){
+        ProductAdapter adapter = new ProductAdapter(this, mProductViewModel,
+                mPageViewModel.getRelatedProducts().getValue(), 0);
+        mBinding.rvRelatedProducts.setAdapter(adapter);
+    }
+
+    private void initViews(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+                getActivity(),
+                LinearLayoutManager.HORIZONTAL,
+                true);
+        mBinding.rvRelatedProducts.setLayoutManager(linearLayoutManager);
     }
 
 }
